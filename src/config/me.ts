1aa7bd57fdb75c2b6d1b37234aa9cb3b74d3f289
Alastair Zotos <alastairzotos@gmail.com>
2023-11-16 19:49:09 +0200

    chore: content

--- src/config/me.ts
+++ src/config/me.ts
@@ -3,7 +3,7 @@ export const meConfig = {
   name: "Alastair",
   tagline: "I develop webapps",
   briefDesc: `
-    Hi, my name is Alastair, the founder of bitmetro.
+    Hi, I'm Alastair, the founder of bitmetro.
     
     I have been developing software since I was 10 years old making games in my bedroom. Since then my interests have grown and I currently work as a full stack software engineer primarily using React and Node.js, with active involvement in DevOps/Infra/CICD. I also build programming languages, virtual assistants, browser plugins, and other curiosities.
   `,
