79cd90a4748cccb248ae479ae050a1dd07a10451
Alastair Zotos <alastairzotos@gmail.com>
2024-08-12 03:11:16 +0300

    Nav UI improvements

--- src/components/new-layout/navigation.tsx
+++ src/components/new-layout/navigation.tsx
@@ -10,31 +10,40 @@ interface Props {
 export const Navigation: React.FC<Props> = ({ headerRef }) => {
   const [menuOpen, setMenuOpen] = useState(false);
   const [scrolledPastHeader, setScrolledPastHeader] = useState(false);
+  const [scrolledFirstThird, setScrolledFirstThird] = useState(false);
 
   useEffect(() => {
-    const height = (headerRef.current?.getBoundingClientRect().height || Number.MAX_SAFE_INTEGER) - 72;
+    const height = (headerRef.current?.getBoundingClientRect().height || Number.MAX_SAFE_INTEGER);
 
     const handleScroll = () => {
-      if (!scrolledPastHeader && window.scrollY >= height) {
+      if (!scrolledPastHeader && window.scrollY >= height - 72) {
         setScrolledPastHeader(true);
       }
 
-      if (scrolledPastHeader && window.scrollY < height) {
+      if (scrolledPastHeader && window.scrollY < height - 72) {
         setScrolledPastHeader(false);
+      }
+
+      if (!scrolledFirstThird && window.scrollY >= height / 3 - 72) {
+        setScrolledFirstThird(true);
+      }
+
+      if (scrolledFirstThird && window.scrollY < height / 3 - 72) {
+        setScrolledFirstThird(false);
       }
     }
 
     window.addEventListener('scroll', handleScroll);
 
     return () => window.removeEventListener('scroll', handleScroll);
-  }, [headerRef.current, scrolledPastHeader]);
+  }, [headerRef.current, scrolledPastHeader, scrolledFirstThird]);
 
   const menuItemClassName = "border-1 border-slate-400 border-solid transition-bg duration-100 ease-in-out bg-black hover:bg-slate-800" + (!scrolledPastHeader ? " min-w-[33%] lg:min-w-[16.6%]" : "");
 
   return (
     <div className="fixed top-0 right-0 left-0 xl:m-4 z-[99999]">
-      <nav className={`w-full flex justify-between border-1 transition duration-300 ease-in-out border-${scrolledPastHeader ? "transparent" : "slate-400"} border-solid`}>
-        <div className={menuItemClassName + ' transition-opacity duration-300 ease-in-out'} style={{ opacity: scrolledPastHeader ? 0.6 : 1 }}>
+      <nav className={`w-full flex justify-between border-1 transition duration-300 ease-in-out border-${scrolledFirstThird ? "transparent" : "slate-400"} border-solid`}>
+        <div className={menuItemClassName + ' transition-opacity duration-300 ease-in-out'} style={{ opacity: scrolledFirstThird ? 0.6 : 1 }}>
           <Link href="/" className="w-full h-full">
             <div className="flex w-full items-center gap-4 m-4">
               <Logo />
